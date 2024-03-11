f03ec94e2a344e1a1f3611075aad05cc8d25e7a3
MathiasEAA <eaamatoj@students.eaaa.dk>
2024-03-11 10:20:11 +0100

    Lavet Ordination forbindelse til Laegemiddel, samt getType i DagligSkaev.java og DagligFast.java

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -30,7 +30,6 @@ public class DagligFast extends Ordination{
 
     @Override
     public String getType() {
-        //TODO
-        return null;
+        return super.laegemiddel.getEnhed();
     }
 }
