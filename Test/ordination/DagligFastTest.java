906a48875ac97086659544b3ccc2fc7814a51898
MathiasEAA <eaamatoj@students.eaaa.dk>
2024-03-20 09:10:23 +0100

    Added methods for DagligFastTest.java

---
+++ Test/ordination/DagligFastTest.java
@@ -0,0 +1,30 @@
+package ordination;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DagligFastTest {
+
+
+    @Test
+    void DagligFast() {
+
+    }
+    @Test
+    void opretDosis() {
+    }
+
+    @Test
+    void samletDosis() {
+    }
+
+    @Test
+    void doegnDosis() {
+    }
+
+    @Test
+    void givDosis() {
+
+    }
+}
