a43675d90b06d6c1d17366f313cdf9ee434cddcc
MathiasEAA <eaamatoj@students.eaaa.dk>
2024-03-20 10:24:32 +0100

    Filled method opretDagligFastOrdination

--- Test/controller/ControllerTest.java
+++ Test/controller/ControllerTest.java
@@ -1,6 +1,12 @@
 package controller;
 
+import ordination.DagligFast;
+import ordination.Laegemiddel;
+import ordination.Patient;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +18,26 @@ class ControllerTest {
 
     @Test
     void opretDagligFastOrdination() {
+        // Arrange
+        Controller controller = Controller.getTestController();
+        Patient patient = controller.opretPatient("123456-7890","Hans Hansen",60);
+        Laegemiddel laegemiddel = controller.opretLaegemiddel("Paracetamol",1,1.5,2,"ml");
+        LocalDate start = LocalDate.of(2024,03,18);
+        LocalDate slut  = LocalDate.of(2024,03,28);
+        DagligFast dagligFast;
+
+        // Act
+        dagligFast = controller.opretDagligFastOrdination(start,slut,patient,laegemiddel,0.25,0.5,0.5,0.25);
+        LocalDate actualStart = dagligFast.getStartDen();
+        LocalDate actualSlut = dagligFast.getSlutDen();
+
+        // Assert
+        assertNotNull(dagligFast);
+        assertNotNull(dagligFast.getLaegemiddel());
+        assertTrue(patient.getOrdinationer().contains(dagligFast));
+        assertEquals(start,actualStart);
+        assertEquals(slut,actualSlut);
+
     }
 
     @Test
